7c1cccb6135feda363482ce5f615f2e8cfdb7c34
katia <113106563+KatiaKosh@users.noreply.github.com>
2022-09-23 10:13:09 +0500

    Update Program.cs

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 13
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2 ? $"{number} -> {numberS[2]}" : $"{numberS} -> третьей цифры нет");
