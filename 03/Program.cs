3993428cc9e27bbb456f3437f237f27c51398d01
katia <113106563+KatiaKosh@users.noreply.github.com>
2022-09-23 10:13:30 +0500

    Update Program.cs

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 15
+
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+string isWeekend = (dayOfWeek > 0 && dayOfWeek < 8) 
+    ? (dayOfWeek == 6 || dayOfWeek == 7) ? "да" : "нет"
+    : "Это не день недели:(";
+
+Console.WriteLine($"{dayOfWeek} -> {isWeekend}");
